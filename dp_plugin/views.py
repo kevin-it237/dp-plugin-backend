3ed3b73bae89b555d4c8e2ec30b0d7a2034f639d
kevin-it237 <ngaleuabel@gmail.com>
2023-03-17 19:14:03 +0100

    event image upload done

--- dp_plugin/views.py
+++ dp_plugin/views.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from . import get_environment
 from werkzeug.utils import secure_filename
 import json
+import base64
 
 from pymisp import MISPEvent, MISPObject, PyMISP
 
@@ -71,31 +72,32 @@ def create_event():
         # Optional, defaults to MISP.default_event_threat_level in MISP config
         event.threat_level_id = 2
         event.analysis = 1  # Optional, defaults to 0 (initial analysis)
-        # event.published = True
-        event.add_attribute(type='url', value=domain)
-        event.add_attribute(type='other', value=location)
+        event.published = True
+        # add datetime attribute
+        event.add_attribute(type='datetime', value=date.today())
         event.set_date(date.today())
         event.add_tag('dark-pattern-1')
+        
         # Add custom object to the event
-        misp_object = MISPObject('custom-dark-pattern')
         # * standalone: this object will be attached to a MISPEvent, so the references will be in the dump
-        misp_object.add_attribute('location',type='text', value=location)
-        misp_object.add_attribute('requirements', type='text', value=requirements)
-        misp_object.add_attribute('strategies', type='text', value=strategies)
-        misp_object.add_attribute('notes', type='text', value=notes)
-        event.add_object(misp_object)
+        dark_pattern_v3_obj = MISPObject(name='dark-pattern-schema-v3')
+        dark_pattern_v3_obj.add_attribute(object_relation='Place_of_publication', type='text', value=location)
+        dark_pattern_v3_obj.add_attribute(object_relation='Regulations', type='text', value=requirements)
+        dark_pattern_v3_obj.add_attribute(object_relation='Design strategies', type='text', value=strategies)
+        # dark_pattern_v3_obj.add_attribute(object_relation='Additional_Info', type='text', value=notes)
+        dark_pattern_v3_obj.add_attribute(object_relation='Business_sector', type='text', value="Public administration")
+        event.add_object(dark_pattern_v3_obj)
 
         # Add an attachment
         # check if the post request has the file part
         file = dict(request.files)['attachment']
+        image_string = base64.b64encode(file.read())
         filename = secure_filename(file.filename)
-        # image_object = MISPObject('image')
-        # image_object.add_attribute("attachment", value=filename, data=file, expand='binary')
-        # image_object.add_attribute("filename", filename)
-        # image_object.add_attribute("md5", file.md5)
-        # image_object.add_attribute("sha1", file.sha1)
-        # image_object.add_attribute("sha256", file.sha256)
-        # event.add_object(image_object)
+        attachment_object = MISPObject(name='image')
+        attachment_object.add_attribute(object_relation="attachment", value=filename, data=image_string)
+        attachment_object.add_attribute(object_relation="url", value=domain)
+        event.add_object(attachment_object)
+        
         event.publish()
         event = misp.add_event(event, pythonify=True)
 
